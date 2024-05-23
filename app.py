1c59170c49d728c6a96c1e184def55e992bc083e
Abdul Rafay <80894764+abdulrafay97@users.noreply.github.com>
2024-05-24 01:30:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def Predict(img):
 
 
 #Get Image
-file_up = st.file_uploader('Upload an Image', type = "tiff")
+file_up = st.file_uploader('Upload an Image', type = "jpg")
 
 #Normalizing
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
